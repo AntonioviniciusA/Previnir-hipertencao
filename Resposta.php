adb84a183ff5360a81b64f736997c66b9704719b
AntonioviniciusA <165342404+AntonioviniciusA@users.noreply.github.com>
2024-04-30 22:12:28 -0300

    upload 30 de abril de 2024

---
+++ Resposta.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $nome = $_GET["nome"];
+    $idade = $_GET["idade"];
+    $genero = $_GET["genero"];
+    $sintomas = $_GET["sintomas"];
+    $fatores_risco = $_GET["fatores_risco"];
+    ?>
+    <form action="">
+        <h1>Formulario enviado</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+  <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+  <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+</svg>
+    </form>
+</body>
+</html>
